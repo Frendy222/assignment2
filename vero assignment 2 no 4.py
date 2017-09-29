cabe032d4a4ca20bed9af330cc13d97d76970e21
Frendy222 <frendy222@gmail.com>
2017-09-29 14:18:54 +0700

    perbaikan

--- vero assignment 2 no 4.py
+++ vero assignment 2 no 4.py
@@ -2,32 +2,51 @@ print('Input in a, b, or c ')
 data_input = input('Input :')
 data = data_input.lower()
 list_data = list(data)
-out = 1
+out = [1,0,0]
+i=0
+
+def typeA():
+    x=out[0]
+    y=out[1]
+    z=out[2]
+    out.remove(x)
+    out.remove(y)
+    out.insert(0,y)
+    out.insert(1,x)
+def typeB():
+    x=out[0]
+    y=out[1]
+    z=out[2]
+    out.remove(y)
+    out.remove(z)
+    out.insert(1,z)
+    out.insert(2,y)
+def typec():
+    x=out[0]
+    y=out[1]
+    z=out[2]
+    out.remove(x)
+    out.remove(z)
+    out.insert(0,z)
+    out.insert(2,x)
 
 for letter in list_data:
-    if letter == "a" :
-       if out == 1 :
-           out += 1
-       elif out == 2:
-           out -= 1
-       else :
-           out = 'error a'
-
-    elif letter == 'b' :
-        if out == 2 :
-           out += 1
-        elif out == 3 :
-            out -= 1
-        else :
-            out = "error b"
-    elif letter == 'c' :
-        if out == 1 :
-           out += 2
-        elif out == 3 :
-            out -= 2
-        else:
-            out = "error c"
+    if letter == 'a':
+        typeA()
+    elif letter == 'b':
+        typeB()
+    elif letter == 'c':
+        typec()
     else :
-        out = 'error input'
+        end = 'Error!!'
+x=out[0]
+y=out[1]
+z=out[2]
 
-print(out)
+if x == 1:
+    end=1
+elif y == 1:
+    end=2
+elif z==1:
+    end=3
+print(end)
